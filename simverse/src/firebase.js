2c078523e2ec1624cd07d5a237cdfbdb14a38819
zainabfatima097 <zainabfatima.zaman@gmail.com>
2026-01-08 21:09:32 +0530

    firebase

--- simverse/src/firebase.js
+++ simverse/src/firebase.js
@@ -1,17 +1,21 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDGy81wJLzeqUuO0bneDOWax6DEtHVemh0",
-  authDomain: "shms-8156d.firebaseapp.com",
-  projectId: "shms-8156d",
-  storageBucket: "shms-8156d.firebasestorage.app",
-  messagingSenderId: "83575466414",
-  appId: "1:83575466414:web:c0d409c0e934aa193b003f",
+  apiKey: "AIzaSyCf8Yx6K2ADAwL_Zrg4prjVfVrLFKQLmrQ",
+  authDomain: "zeee-dd466.firebaseapp.com",
+  projectId: "zeee-dd466",
+  storageBucket: "zeee-dd466.firebasestorage.app",
+  messagingSenderId: "422400124405",
+  appId: "1:422400124405:web:9f7cb2aaa528d4aa72ecbc",
+  measurementId: "G-7GDLJ5T4NY"
 };
 
 const app = initializeApp(firebaseConfig);
 
+// ✅ EXPORT THESE
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+export const analytics = getAnalytics(app);
